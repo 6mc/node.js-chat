695b14c4a10ed65b664a8ab92d12fc8dface3f6e
Mehmet Can <super.can@bk.ru>
2020-06-28 18:35:02 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,59 @@
+Support chat
+============
+
+A simple chat system with private messages, using Node.js and Socket.io
+
+Features:
+
+* Realtime user join/quit events
+* Chat to the whole channel or private chat to a single user
+* Connected users list
+* Anonymous usernames
+
+![Support Chat](http://i.imgur.com/kJ7RyNR.png "Support Chat")
+
+Requirements
+------------
+
+* [Node.js](http://nodejs.org/)
+* [Npm](http://npmjs.org/)
+
+Modules:
+
+* [Socket.io](http://socket.io/)
+* [Express](http://expressjs.com/)
+
+Installation
+----------
+
+1. Clone the repository with ``git clone git://github.com/Fabryz/support-chat.git support-chat``
+2. Install dependencies with ``npm install``
+3. Start the server with ``node server.js`` or ``nodemon server.js`` if you have it
+4. Point your browser to ``YOUR_SERVER_IP:8080``
+5. Your support chat is ready!
+
+License
+-------
+
+MIT License
+
+Copyright (c) 2013 Fabrizio Codello
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- server.js
+++ server.js
@@ -1,14 +1,96 @@
-var app = require('http').createServer()
-var io =require('socket.io')(app);
+/*
+* Express
+*/
 
-app.listen(8000);
+var express = require('express'),
+    socketio = require('socket.io'),
+    http = require('http'),
+    path = require('path');
 
+var app = express();
 
-io.on('connection',function (socket) {
-    var alert = 'hello from the other side' + math.random();
+// Configuration
 
-    socket.on ('disconnect',function () {
-        console.log('1 client disconnected');
-
-    });
+app.configure(function(){
+    app.set('port', process.env.PORT || 8080);
+    app.use(express.favicon());
+    app.use(express.logger('short'));
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.set("view engine", "ejs");
+    app.use("/", express.static(__dirname + "/views/"));
 });
+
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', function(req, res) {
+    res.sendfile('public/index.html');
+});
+
+app.get('/private/:user1/:user2', function(req, res) {
+    //res.sendfile('public/index.html');
+    res.render('v2.ejs',{sender: req.params.user1 , receiver: req.params.user2})
+});
+
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port "+ app.get('port') +" in "+ app.get('env') +" mode.");
+});
+
+/*
+* Socket.IO
+*/
+
+var io = socketio.listen(server);
+
+io.configure(function() {
+    io.enable('browser client minification');
+    io.set('log level', 1);
+});
+
+//var users = [];
+   var users = { };
+
+io.sockets.on('connection', function(client) {
+
+ 
+
+      client.on("create", function(data) {
+      //users.push(data.usr);
+        users[data.usr] = client.id;
+    });
+
+
+    console.log(Object.keys(users));
+
+    console.log('+ User '+ client.id +' connected ('+ client.handshake.address.address +'). Total users: '+ Object.keys(users).length );
+
+    // users.Keys().length); //Object.keys(users).length );
+
+    client.emit("nick", { nick: client.id });
+
+    io.sockets.emit("users", { users: Object.keys(users) });
+
+    client.on("chat", function(data) {
+        io.sockets.emit("chat", { from: client.id, msg: data.msg });
+    });
+
+    client.on("private", function(data) {
+        io.sockets.sockets[users[data.to]].emit("private", { from: client.id, to: data.to, msg: data.msg });
+        client.emit("private", { from: client.id, to: data.to, msg: data.msg });
+    });
+
+
+
+
+
+});
+
